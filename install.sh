c7134c3ecb49cd699ff0a9161d4838320af66d23
Jack <jack@JackdeMBP.lan>
2016-11-17 21:00:23 +0800

    fix dependency problem for lastest tmux installation on ubuntu

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ export EDITOR=vim
 echo "export EDITOR=vim" >> ~/.bashrc
 
 # install libevent, dependency of tmux
-sudo apt-get install libevent-2.0-5
+sudo apt-get update
+sudo apt-get install build-essential libevent-2.0-5 libevent-dev libncurses5-dev autoconf automake m4 libtool perl pkg-config
 
 # install tmux
 cd ~
@@ -14,6 +15,7 @@ git clone https://github.com/tmux/tmux.git
 cd tmux
 sh autogen.sh
 ./configure && make
+sudo make install
 
 # quick tmux config
 cd ~
