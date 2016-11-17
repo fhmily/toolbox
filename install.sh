2d20dc9a3da81fe536fc5a7dc6124ee5efc8fa5c
Jack <jack@JackdeMBP.lan>
2016-11-17 20:47:55 +0800

    install tmux update

--- install.sh
+++ install.sh
@@ -4,8 +4,20 @@
 export EDITOR=vim
 echo "export EDITOR=vim" >> ~/.bashrc
 
+# install libevent, dependency of tmux
+sudo apt-get install libevent-2.0-5
+
+# install tmux
+cd ~
+rm -rf tmux
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
+
 # quick tmux config
 cd ~
+rm -rf .tmux
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+cp -f .tmux/.tmux.conf.local .
